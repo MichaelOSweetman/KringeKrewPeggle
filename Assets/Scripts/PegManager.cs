69da2f7f7c241d94d67c61de4ae4e0170dcf141a
Michael <michaelsweetman888@gmail.com>
2023-10-16 01:03:22 +1100

    A set number of random pegs are now assigned to start as green/orange
    Changed bluePegs to be a list called activeBluePegs rather than an array
    Added a SetPegType function that sets a peg's internal type, as well as its colour
    Pegs not set as green or orange are now added to the activeBluePegs list
    Blue pegs are now removed from the activeBluePegs list when hit
    Added a ReplacePurplePeg function to set a blue peg to purple each 'phase' and returning the previously purple peg back to blue
    The purple peg is replaced when the hitPegs queue is cleared
    The ClearHitPegs function now only functions if at least peg was hit
    Time Spent This Week: 02:53:18

--- Assets/Scripts/PegManager.cs
+++ Assets/Scripts/PegManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
     File name: PegManager.cs
     Summary: Manages a set of pegs and determines which are orange, purple, green and blue. It also determines the amount of points they give, as well as when they are removed as a result of being hit
     Creation Date: 09/10/2023
-    Last Modified: 09/10/2023
+    Last Modified: 16/10/2023
 */
 public class PegManager : MonoBehaviour
 {
@@ -31,13 +31,88 @@ public class PegManager : MonoBehaviour
     public int m_startingGreenPegCount = 2;
     Peg[] m_activePegs;
     Queue<Peg> m_hitPegs;
-    Peg[] m_bluePegs;
+    List<Peg> m_activeBluePegs;
     Peg m_purplePeg = null;
 
     public float m_clearHitPegDelay = 0.25f;
     bool m_clearHitPegQueue = false;
     float m_clearHitPegQueueTimer = 0.0f;
 
+    public void SetPegType(Peg a_peg, PegType a_pegType, bool a_hit)
+    {
+        // set the peg's pegtype
+        a_peg.m_pegType = a_pegType;
+        
+        // if the peg was hit
+        if (a_hit)
+        {
+            // set the peg's colour to the hit version of its colour
+            switch (a_peg.m_pegType)
+            {
+                case PegType.Blue:
+                    a_peg.GetComponent<SpriteRenderer>().color = m_hitBluePegColor;
+                    break;
+                case PegType.Orange:
+                    a_peg.GetComponent<SpriteRenderer>().color = m_hitOrangePegColor;
+                    break;
+                case PegType.Purple:
+                    a_peg.GetComponent<SpriteRenderer>().color = m_hitPurplePegColor;
+                    break;
+                case PegType.Green:
+                    a_peg.GetComponent<SpriteRenderer>().color = m_hitGreenPegColor;
+                    break;
+            }
+        }
+        // if the peg was not hit
+        else
+        {
+            // set the peg's colour to the base version of its colour
+            switch (a_peg.m_pegType)
+            {
+                case PegType.Blue:
+                    a_peg.GetComponent<SpriteRenderer>().color = m_bluePegColor;
+                    break;
+                case PegType.Orange:
+                    a_peg.GetComponent<SpriteRenderer>().color = m_orangePegColor;
+                    break;
+                case PegType.Purple:
+                    a_peg.GetComponent<SpriteRenderer>().color = m_purplePegColor;
+                    break;
+                case PegType.Green:
+                    a_peg.GetComponent<SpriteRenderer>().color = m_greenPegColor;
+                    break;
+            }
+        }
+    }
+
+    public void ReplacePurplePeg()
+    {
+        // if there are active blue pegs remaining
+        if (m_activeBluePegs.Count > 0)
+        {
+            // store the peg that was previously purple
+            Peg previousPurplePeg = m_purplePeg;
+            // get a random blue peg
+            int randomBluePegID = Random.Range(0, m_activeBluePegs.Count);
+
+            // set the purple peg to be a random blue peg
+            m_purplePeg = m_activeBluePegs[randomBluePegID];
+            // remove the peg from this list of blue pegs
+            m_activeBluePegs.RemoveAt(randomBluePegID);
+
+            // if there was a previous purple peg and it is still active
+            if (previousPurplePeg && previousPurplePeg.transform.gameObject.activeSelf)
+            {
+                // set the previous purple peg to be blue
+                SetPegType(previousPurplePeg, PegType.Blue, false);
+                // add the peg back to the list of active blue pegs
+                m_activeBluePegs.Add(previousPurplePeg);
+            }
+
+            // set the peg assigned to be purple to be purple
+            SetPegType(m_purplePeg, PegType.Purple, false);
+        }
+    }
 
     public void ResolveHit(int a_pegID)
     {
@@ -47,32 +122,26 @@ public class PegManager : MonoBehaviour
             // add the peg to the hit pegs queue
             m_hitPegs.Enqueue(m_activePegs[a_pegID]);
 
-            // set the peg's color to the hit version
-            switch (m_activePegs[a_pegID].m_pegType)
-            {
-                case PegType.Blue:
-                    m_activePegs[a_pegID].GetComponent<SpriteRenderer>().color = m_hitBluePegColor;
-                    break;
-                case PegType.Orange:
-                    m_activePegs[a_pegID].GetComponent<SpriteRenderer>().color = m_hitOrangePegColor;
-                    break;
-                case PegType.Purple:
-                    m_activePegs[a_pegID].GetComponent<SpriteRenderer>().color = m_hitPurplePegColor;
-                    break;
-                case PegType.Green:
-                    m_activePegs[a_pegID].GetComponent<SpriteRenderer>().color = m_hitGreenPegColor;
-                    break;
-            }
+            // set the peg's colour to the hit version of its colour
+            SetPegType(m_activePegs[a_pegID], m_activePegs[a_pegID].m_pegType, true);
+            
+            // remove the peg from the list of active blue pegs if it was in it
+            m_activeBluePegs.Remove(m_activePegs[a_pegID]);
 
             // remove the peg from the active peg array
             m_activePegs[a_pegID] = null;
+
         }
     }
 
     public void ClearHitPegs()
     {
-        // set the clear hit peg queue flag to true so the queue starts emptying
-        m_clearHitPegQueue = true;
+        // if at least 1 peg was hit
+        if (m_hitPegs.Count > 0)
+        {
+            // set the clear hit peg queue flag to true so the queue starts emptying
+            m_clearHitPegQueue = true;
+        }
     }
 
     // Start is called before the first frame update
@@ -80,15 +149,50 @@ public class PegManager : MonoBehaviour
     {
         // create an array the size of the amount of children the peg manager has
         m_activePegs = new Peg[transform.childCount];
+        // create a list to store all active blue pegs
+        m_activeBluePegs = new List<Peg>();
         // initialise the hit pegs queue
         m_hitPegs = new Queue<Peg>();
+
+        // create a hash set to store the IDs of all pegs that will start as orange or green
+        HashSet<int> orangeAndGreenPegIDs = new HashSet<int>();
+        // create an integer variable to store the ID for the peg to be added to the hash set
+        int randomPegID = 0;
+        // create an integer to act as a tracker for how many orange pegs have been added
+        
+        // loop for each starting orange and green peg
+        for (int i = 0; i < m_startingOrangePegCount + m_startingGreenPegCount; ++i)
+        {
+            // get a random peg ID that has not yet been assigned to a peg
+            do
+            {
+                randomPegID = Random.Range(0, transform.childCount);
+            }
+            while (orangeAndGreenPegIDs.Contains(randomPegID));
+        
+            // add the ID to to the hash set
+            orangeAndGreenPegIDs.Add(randomPegID);
+            // add the peg to the active pegs array
+            m_activePegs[randomPegID] = transform.GetChild(randomPegID).gameObject.GetComponent<Peg>();
+            // set the peg's type to orange, or green if all orange pegs have been assigned
+            SetPegType(m_activePegs[randomPegID], (i < m_startingOrangePegCount) ? PegType.Orange : PegType.Green, false);
+        }
 
         // loop for each child
         for (int i = 0; i < transform.childCount; ++i)
         {
-            // add the child to the list of active pegs
-            m_activePegs[i] = transform.GetChild(i).gameObject.GetComponent<Peg>();
+            // if the peg has not yet been assigned, it is should start as a blue peg
+            if (m_activePegs[i] == null)
+            {
+                // add the child to the array of active pegs
+                m_activePegs[i] = transform.GetChild(i).gameObject.GetComponent<Peg>();
+                // add the peg to the list of active blue pegs
+                m_activeBluePegs.Add(m_activePegs[i]);
+            }
         }
+
+        // assign a random blue peg to be purple
+        ReplacePurplePeg();
     }
 
     // Update is called once per frame
@@ -119,6 +223,8 @@ public class PegManager : MonoBehaviour
                 {
                     // set the clear hit peg queue flag to false
                     m_clearHitPegQueue = false;
+                    // assign a random blue peg to be purple
+                    ReplacePurplePeg();
                 }
             }
         }
