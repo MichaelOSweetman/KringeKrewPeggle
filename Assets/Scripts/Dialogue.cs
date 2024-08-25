9821c1b1aa8e03b6f6399058f3f80ed553267cee
Michael <michaelsweetman888@gmail.com>
2024-08-25 23:34:05 +1000

    Restructured Dialogue.cs structs and lists to work with the Inspector
    Fixed bugs and syntax errors introduced in the previous programming session that occurred as a result of lack of access to Unity/Visual Studio
    Created the backdrop for the Dialogue Screen
    renamed m_characterDelay to m_letterDelay to prevent confusion regarding playable characters
    
    Time Spent This Week: 01:20:25

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -7,31 +7,40 @@ using UnityEngine.UI;
     File name: Dialogue.cs
     Summary: Procedurely fills a text box with a message, moving to the next message when prompted by the player
     Creation Date: 10/06/2024
-    Last Modified: 19/08/2024
+    Last Modified: 25/08/2024
 */
 public class Dialogue : MonoBehaviour
 {
-	public struct Character
+	[System.Serializable] public struct Character
 	{
-		SpriteRenderer m_portrait;
-		Sprite[] m_expressions;
+		public string m_title;
+		public Texture[] m_expressions;
 	}
 
-	public struct Message
+	[System.Serializable] public struct Message
 	{
-		Character m_character;
-		string m_message;
-		int m_expressionID;
+		public int m_characterID;
+		public string m_message;
+		public int m_expressionID;
 	}
+
+	[System.Serializable] public struct MessageSet
+	{
+		public Message[] m_messages;
+	}
+
 
 	Text m_dialogueTextBox;
 	public PegManager m_pegManager;
-	public List<List<Message>> m_dialogueSets;
-	[HideInInspector] public int m_dialogueIndex = 0;
+	public RawImage m_characterPortrait;
+	public Text m_speakerText;
 	public float m_generationSpeed = 0.2f;
-	float m_characterDelay = 0.0f;
 	public bool m_generateEachMessageOverSameDuration = false;
 	public bool m_loadNextLevelOnComplete = true;
+	public List<Character> m_characters;
+	public List<MessageSet> m_dialogueSets;
+	[HideInInspector] public int m_dialogueIndex = 0;
+	float m_letterDelay = 0.0f;
 	float m_timer;
 	int m_messageIndex = 0;
 	int m_characterIndex = 0;
@@ -43,26 +52,26 @@ public class Dialogue : MonoBehaviour
 		// get the text component on this textbox
 		m_dialogueTextBox = GetComponent<Text>();
 
-		// determine the delay between characters appearing depending on if m_generationSpeed should represent the time for the full message to appear or each character
-		m_characterDelay = (m_generateEachMessageOverSameDuration) ? m_dialogueSets[m_dialogueIndex][m_messageIndex].m_message.Length / m_generationSpeed : m_generatationSpeed;
-    }
+		// determine the delay between letters appearing depending on if m_generationSpeed should represent the time for the full message to appear or each character
+		m_letterDelay = (m_generateEachMessageOverSameDuration) ? m_dialogueSets[m_dialogueIndex].m_messages[m_messageIndex].m_message.Length / m_generationSpeed : m_generationSpeed;
+	}
 
     // Update is called once per frame
     void Update()
     {
 		// if there is a message to display and it isn't already finished
-		if (m_dialogueSets[m_dialogueIndex][m_messageIndex].m_message.Length > 0 && m_dialogueTextBox.text.Length < m_dialogueSets[m_dialogueIndex][m_messageIndex].m_message.Length)
+		if (m_dialogueSets[m_dialogueIndex].m_messages[m_messageIndex].m_message.Length > 0 && m_dialogueTextBox.text.Length < m_dialogueSets[m_dialogueIndex].m_messages[m_messageIndex].m_message.Length)
 		{
 			// update the timer
 			m_timer += Time.fixedDeltaTime;
 			
 			// if enough time has passed
-			if (m_timer >= m_characterDelay)
+			if (m_timer >= m_letterDelay)
 			{
 				// reset the timer
-				m_timer -= m_generationSpeed;
+				m_timer -= m_letterDelay;
 				// add the next character to the text box
-				m_dialogueTextBox.text += m_dialogueSets[m_dialogueIndex][m_messageIndex].m_message[m_characterIndex];
+				m_dialogueTextBox.text += m_dialogueSets[m_dialogueIndex].m_messages[m_messageIndex].m_message[m_characterIndex];
 				++m_characterIndex;
 			}
 		}
@@ -71,13 +80,13 @@ public class Dialogue : MonoBehaviour
 		if (Input.GetButtonDown("Shoot / Use Power"))
 		{
 			// if the message isn't finished
-			if (m_dialogueTextBox.text.Length < m_dialogueSets[m_dialogueIndex][m_messageIndex].m_message.Length)
+			if (m_dialogueTextBox.text.Length < m_dialogueSets[m_dialogueIndex].m_messages[m_messageIndex].m_message.Length)
 			{
 				// instantly complete the message
-				m_dialogueTextBox.text = m_dialogueSets[m_dialogueIndex][m_messageIndex].m_message;
+				m_dialogueTextBox.text = m_dialogueSets[m_dialogueIndex].m_messages[m_messageIndex].m_message;
 			}
 			// otherwise, if there are no more messages to display
-			else if (m_messageIndex == m_dialogueSets[m_dialogueIndex].Count - 1)
+			else if (m_messageIndex == m_dialogueSets[m_dialogueIndex].m_messages.Length - 1)
 			{
 				// if the next level should be loaded now that the dialogue has completed
 				if (m_loadNextLevelOnComplete)
@@ -92,9 +101,6 @@ public class Dialogue : MonoBehaviour
 			// otherwise, if there are more messages to display and the current message is finished
 			else
 			{
-				// set the current speaking character's expression to the default
-				m_dialogueSets[m_dialogueIndex][m_messageIndex].m_character.m_portrait.sprite = m_dialogueSets[m_dialogueIndex][m_messageIndex].m_character.m_expressions[0];
-
 				// clear the text box
 				m_dialogueTextBox.text = "";
 				// reset the character index
@@ -103,10 +109,13 @@ public class Dialogue : MonoBehaviour
 				++m_messageIndex;
 
 				// determine the delay between characters appearing depending on if m_generationSpeed should represent the time for the full message to appear or each character
-				m_characterDelay = (m_generateEachMessageOverSameDuration) ? m_dialogueSets[m_dialogueIndex][m_messageIndex].m_message.Length / m_generationSpeed : m_generatationSpeed;
+				m_letterDelay = (m_generateEachMessageOverSameDuration) ? m_dialogueSets[m_dialogueIndex].m_messages[m_messageIndex].m_message.Length / m_generationSpeed : m_generationSpeed;
+
+				// update the speaker text
+				m_speakerText.text = m_characters[m_dialogueSets[m_dialogueIndex].m_messages[m_messageIndex].m_characterID].m_title;
 
 				// set the expression of the character speaking to the corresponding expression
-				m_dialogueSets[m_dialogueIndex][m_messageIndex].m_character.m_portrait.sprite = m_dialogueSets[m_dialogueIndex][m_messageIndex].m_character.m_expressions[m_dialogueSets[m_dialogueIndex][m_messageIndex].m_expressionID];
+				m_characterPortrait.texture = m_characters[m_dialogueSets[m_dialogueIndex].m_messages[m_messageIndex].m_characterID].m_expressions[m_dialogueSets[m_dialogueIndex].m_messages[m_messageIndex].m_expressionID];
 			}
 		}
     }
