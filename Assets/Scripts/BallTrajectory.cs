c9c9a8e4aeb3496cb8f24d54d32064dd692b3306
Michael Sweetman <michaelsweetman888@gmail.com>
2024-12-09 02:25:47 +1100

    The ball trajectory line now stops early if it collides with an object
    Renamed m_DistanceBetweenPoints to m_timeStep to be more accurately named
    The line renderer's position count is now reset each time the line is created
    A circle cast is now created as the line is drawn to check for collisions the ball would have along the path
    If a collision occurs, the line renderer's point count is reduced and the final point is set to the ball's final location upon collision
    m_timeStep currently uses the float "10.0f" to correctly initialise its value. The need for this requires further investigating
    
    Time Spent This Week: 01:27:03

--- Assets/Scripts/BallTrajectory.cs
+++ Assets/Scripts/BallTrajectory.cs
@@ -6,21 +6,24 @@ using UnityEngine;
     File name: BallTrajectory.cs
     Summary: Uses a line renderer to show the player the predicted path of the ball if it were fired this frame
     Creation Date: 02/12/2024
-    Last Modified: 02/12/2024
+    Last Modified: 09/12/2024
 */
 public class BallTrajectory : MonoBehaviour
 {
     public int m_linePointCount = 10;
-    public float m_maxLineLength = 10.0f;
+    public float m_maxLineLength = 4.0f;
     public Transform m_ballSpawnPoint;
     public Rigidbody2D m_ballRigidbody;
     LineRenderer m_lineRenderer;
     Vector2 m_startVelocity = Vector2.zero;
-    float m_distanceBetweenPoints = 0.0f;
+    float m_timeStep = 0.0f;
     Vector2 m_launchDirection = Vector2.zero;
 
     public void CreateTrajectoryLine(float a_ballLaunchSpeed)
     {
+        // set the point count of the line to its default
+        m_lineRenderer.positionCount = m_linePointCount;
+
         // set the first point of the line to the spawn point of the ball
         m_lineRenderer.SetPosition(0, m_ballSpawnPoint.position);
 
@@ -34,12 +37,25 @@ public class BallTrajectory : MonoBehaviour
         // loop for each point to be placed along the line, starting at the second point
         for (int i = 1; i < m_linePointCount; ++i)
         {
-
             // use the following formula to determine the position of the next point
             // Displacement = InitialVelocity * Time + 0.5 * Acceleration * Time * Time
             // S = ut + ½at²
-            m_lineRenderer.SetPosition(i, (Vector2)m_ballSpawnPoint.position + m_startVelocity * (i * m_distanceBetweenPoints) + 0.5f * Physics2D.gravity * (i * m_distanceBetweenPoints) * (i * m_distanceBetweenPoints));
+            m_lineRenderer.SetPosition(i, (Vector2)m_ballSpawnPoint.position + m_startVelocity * (i * m_timeStep) + 0.5f * Physics2D.gravity * (i * m_timeStep) * (i * m_timeStep));
+
+            // create a circlecast, with the width of the ball's diameter, between this point and the previous point
+            RaycastHit2D hit = Physics2D.CircleCast(m_lineRenderer.GetPosition(i - 1), m_ballRigidbody.transform.localScale.x * 0.5f, (m_lineRenderer.GetPosition(i) - m_lineRenderer.GetPosition(i - 1)).normalized, (m_lineRenderer.GetPosition(i) - m_lineRenderer.GetPosition(i - 1)).magnitude);
+            // if the raycast hit a collider
+            if (hit)
+            {
+                // set the position of this point to the position the ball would be when it hits the collider
+                m_lineRenderer.SetPosition(i, hit.centroid);
+                // adjust the point count of the line renderer now that this is the final point
+                m_lineRenderer.positionCount = i + 1;
+                // exit the loop
+                return;
+            }
         }
+
     }
 
     public void ShowLine(bool a_showLine)
@@ -54,16 +70,10 @@ public class BallTrajectory : MonoBehaviour
         // get the line renderer component from this gameobject
         m_lineRenderer = GetComponent<LineRenderer>();
 
-        // initialise the line renderer with the amount of positions along the line
-        m_lineRenderer.positionCount = m_linePointCount;
-
-        // determine the distance between each point along the line given the amount of points and the max length of the line
-        m_distanceBetweenPoints = m_maxLineLength / m_linePointCount;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // determine the amount of time the ball will travel along it's path before the next point is created, given the amount of points and the max length of the line
+        m_timeStep = m_maxLineLength / m_linePointCount / 10.0f;
+        print("point count: " + m_linePointCount);
+        print("max length: " + m_maxLineLength);
+        print("point distance: " + m_timeStep);
     }
 }
