7dbfd3df9cc5720a49c2fbffa9cc65d5fe4ae7ec
Michael Sweetman <michaelsweetman888@gmail.com>
2025-06-09 01:03:30 +1000

    Continued work on SashaPower
    Fixed bugs and syntax errors introduced in the previous programming session that occurred as a result of lack of access to Unity/Visual Studio
    Incorporated SashaPower into the system with a character prefab and a selectable button on the character select menu
    Shifted to moving the pegs via the transform rather than a rigidbody
    Peg Container default position is now stored and is now returned to that position upon reload
    Added temporary system to track if the time is correct to move the pegs, while music is not yet implemented
    Pegs now move in the direction of the specified input, at a distance specified by the m_moveDistance variable
    Fixed issue where timer would be reset as soon as it was within valid bounds rather than when the pegs were moved
    
    Time Spent This Week: 00:51:59

--- Assets/Scripts/GreenPegPowers/SashaPower.cs
+++ Assets/Scripts/GreenPegPowers/SashaPower.cs
@@ -6,21 +6,31 @@ using UnityEngine;
         File name: SashaPower.cs
         Summary: Manages the power gained from the green peg when playing as Sasha
         Creation Date: 01/06/2025
-        Last Modified: 02/06/2025
+        Last Modified: 09/06/2025
 */
 public class SashaPower : GreenPegPower
 {
-    Rigidbody2D m_pegContainer;
+    Transform m_pegContainer;
     Vector3 m_displacement = Vector3.zero;
     public float m_TEMP = 0.25F;
     float m_timer = 0.0f;
     public float m_beatDelay = 0.5f;
     public float m_gracePeriod = 0.1f;
+    Vector3 m_containerDefaultPosition = Vector3.zero;
+    public float m_moveDistance = 1.0f;
+
+    // TEMP
+    SpriteRenderer m_TEMPbeatShowcase;
 
     public override void Initialize()
     {
-        // get the rigidbody from the pegmanager's gameobject
-        m_pegContainer = m_playerControls.m_pegManager.GetComponent<Rigidbody2D>();
+        // get from the pegmanager gameobject's transform
+        m_pegContainer = m_playerControls.m_pegManager.transform;
+        // store its current position as the default position
+        m_pegContainer.transform.position = m_containerDefaultPosition;
+
+        // TEMP
+        m_TEMPbeatShowcase = m_playerControls.m_UIManager.m_launcherRotation.GetComponent<SpriteRenderer>();
     }
 
     public override void Trigger(Vector3 a_greenPegPosition)
@@ -31,7 +41,7 @@ public class SashaPower : GreenPegPower
         // play music
     }
 
-    public override void OnShoot()
+    public override bool OnShoot()
     {
         // play music
 
@@ -53,6 +63,8 @@ public class SashaPower : GreenPegPower
     {
         // reset the power charges
         ResetPowerCharges();
+        // ensure the peg container is at the default position
+        m_pegContainer.transform.position = m_containerDefaultPosition;
     }
 
     public override void Update()
@@ -60,46 +72,60 @@ public class SashaPower : GreenPegPower
         // if the ball is in play
         if (m_playerControls.m_ballInPlay && m_powerCharges > 0)
         {
+            // TEMP
+            m_TEMPbeatShowcase.color = Color.magenta;
+
             m_timer += Time.fixedDeltaTime;
+
+            // TEMP
+            print(m_timer);
+
             // if the down beat has just occured in the music
             if (m_timer >= m_beatDelay - 0.5f * m_gracePeriod && m_timer <= m_beatDelay + 0.5f * m_gracePeriod)
             {
+                // TEMP
+                m_TEMPbeatShowcase.color = Color.yellow;
+
                 // if the pegs are at their base position
-                if (m_displacement.SqrMagnitude < m_TEMP)
+                if ((m_pegContainer.position - m_containerDefaultPosition).sqrMagnitude < m_TEMP)
                 {
                     // if the pegs should move up
                     if (Input.GetAxis("Shoot Isaac's Tears Vertical") > 0)
                     {
-
+                        m_displacement = Vector3.up * m_moveDistance;
                     }
-                    // otherwise, if the pegs should move up
+                    // otherwise, if the pegs should move down
                     else if (Input.GetAxis("Shoot Isaac's Tears Vertical") < 0)
                     {
-
+                        m_displacement = Vector3.down * m_moveDistance;
                     }
                     // otherwise, if the pegs should move left
                     else if (Input.GetAxis("Shoot Isaac's Tears Horizontal") < 0)
                     {
-
+                        m_displacement = Vector3.left * m_moveDistance;
                     }
                     // otherwise, if the pegs should move right
                     else if (Input.GetAxis("Shoot Isaac's Tears Horizontal") > 0)
                     {
+                        m_displacement = Vector3.right * m_moveDistance;
+                    }
 
+                    // TEMP
+                    if (m_displacement.sqrMagnitude > m_TEMP)
+                    { 
+                        // apply the displacement
+                        m_pegContainer.position += m_displacement;
+                        // reset the timer for the next beat
+                        m_timer -= m_beatDelay;
                     }
                 }
-                else
-                {
-                    // TEMP - use displacement vector to return to centre
-                }
-                // reset the timer for the next beat
-                m_timer -= m_beatDelay
             }
             // otherwise, if the timer has surpassed the beat
             else if (m_timer > m_beatDelay + m_gracePeriod * 0.5f)
             {
                 // reset the timer for the next beat
-                m_timer -= m_beatDelay
+                m_timer -= m_beatDelay;
             }
         }
     }
+}
