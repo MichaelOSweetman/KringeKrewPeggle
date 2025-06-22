985d74c66b9442c877cc0980c6a5c8ec891de6e3
Michael Sweetman <michaelsweetman888@gmail.com>
2025-06-23 03:58:56 +1000

    Continued work on SashaPower
    Power charges are now deducted when the turn is resolved
    Renamed Move Isaac Vertical/Horizontal to Use Power Vertical/Horizontal Primary
    Renamed Use Power Vertical/Horizontal to Use Power Vertical/Horizontal Secondary
    Sasha Power now accepts the Primary or Secondary Use Power axes as inputs
    The UI arrow is now hidden while the power is not active
    The UI arrow now rotates based on the input direction
    The UI arrow now has its texture changed when no direction input is detected
    Fixed bug where lerp completion was incorrectly calculated
    The timer no longer resets itself to 0 when the lerp completes
    Added a temporary music system for testing
    
    Time Spent This Week: 01:04:56

--- Assets/Scripts/GreenPegPowers/SashaPower.cs
+++ Assets/Scripts/GreenPegPowers/SashaPower.cs
@@ -7,12 +7,14 @@ using UnityEngine.UI;
         File name: SashaPower.cs
         Summary: Manages the power gained from the green peg when playing as Sasha
         Creation Date: 01/06/2025
-        Last Modified: 16/06/2025
+        Last Modified: 23/06/2025
 */
 public class SashaPower : GreenPegPower
 {
     public GameObject m_UIArrowPrefab;
+    public Texture m_inactiveArrowTexture;
     RawImage m_UIArrow;
+    Texture m_activeArrowTexture;
     public float m_beatDelay = 0.5f;
     public float m_gracePeriod = 0.25f;
     public float m_moveDistance = 1.0f;
@@ -23,10 +25,41 @@ public class SashaPower : GreenPegPower
     bool m_atDefaultPosition = true;
     float m_timer = 0.0f;
 
+    // TEMP
+    AudioSource m_audioSource;
+    float m_musicTimer = 0.0f;
+    bool m_music = false;
+    bool m_up = true;
+
+    public void TEMPMUSIC()
+    {
+        m_musicTimer += Time.unscaledDeltaTime;
+        if (m_up && m_musicTimer > m_beatDelay)
+        {
+            m_audioSource.pitch = 0.25f;
+            m_audioSource.Play();
+            m_musicTimer -= m_beatDelay;
+            m_up = false;
+        }
+        else if (!m_up && m_musicTimer > m_beatDelay * 0.5f)
+        {
+            m_audioSource.pitch = 0.5f;
+            m_audioSource.Play();
+            m_up = true;
+        }
+    }
+
     public override void Initialize()
     {
+        // TEMP
+        m_audioSource = m_playerControls.m_UIManager.m_musicAudioSource;
+
         // create the ui arrow and set its parent to be the parent of the power charges text so they are on the canvas
         m_UIArrow = Instantiate(m_UIArrowPrefab, m_powerChargesText.rectTransform.parent).GetComponent<RawImage>();
+        // store the arrow's current texture as the active texture
+        m_activeArrowTexture = m_UIArrow.texture;
+        // hide the UI Arrow
+        m_UIArrow.gameObject.SetActive(false);
 
         // get from the pegmanager gameobject's transform
         m_pegContainer = m_playerControls.m_pegManager.transform;
@@ -39,19 +72,17 @@ public class SashaPower : GreenPegPower
         // add the charges
         ModifyPowerCharges(m_gainedPowerCharges);
 
+        // show the UI Arrow
+        m_UIArrow.gameObject.SetActive(true);
+
         // play music
+        m_music = true;
     }
 
     public override bool OnShoot()
     {
-        // if there are power charges
-        if (m_powerCharges > 0)
-        {
-            // remove one
-            ModifyPowerCharges(-1);
-        }
-
         // play music
+        m_music = true;
 
         // return that this function should not override the default shoot function
         return false;
@@ -59,6 +90,24 @@ public class SashaPower : GreenPegPower
 
     public override void ResolveTurn()
     {
+        // if there are power charges
+        if (m_powerCharges > 0)
+        {
+            // remove one
+            ModifyPowerCharges(-1);
+
+            // if there are now zero power charges
+            if (m_powerCharges == 0)
+            {
+                // hide the UI arrow
+                m_UIArrow.gameObject.SetActive(false);
+            }
+
+        }
+
+        // stop the music
+        //TEMP m_music = false;
+
         // ensure the pegs are at their default position
         m_pegContainer.position = m_containerDefaultPosition;
         m_atDefaultPosition = true;
@@ -75,51 +124,68 @@ public class SashaPower : GreenPegPower
         m_pegContainer.transform.position = m_containerDefaultPosition;
         m_atDefaultPosition = true;
 
+        // stop the music
+        //TEMP m_music = false;
+
         // temp
         m_UIArrow.color = Color.white;
     }
 
     public override void Update()
     {
+        // TEMP
+        if (m_music)
+        {
+            TEMPMUSIC();
+        }
+
         // if the ball is in play and there are power charges
         if (true)//m_playerControls.m_ballInPlay && m_powerCharges > 0)
         {
             // increase the timer
             m_timer += Time.unscaledDeltaTime;
 
+            // temp
+            m_UIArrow.texture = m_activeArrowTexture;
+
             // determine the direction the pegs should move
-            if (Input.GetAxis("Use Power Vertical") > 0)
+            if (Input.GetAxis("Use Power Vertical Primary") > 0 || Input.GetAxis("Use Power Vertical Secondary") > 0)
             {
                 m_direction = Vector3.up;
             }
-            else if (Input.GetAxis("Use Power Vertical") < 0)
+            else if (Input.GetAxis("Use Power Vertical Primary") < 0 || Input.GetAxis("Use Power Vertical Secondary") < 0)
             {
                 m_direction = Vector3.down;
             }
-            else if (Input.GetAxis("Use Power Horizontal") < 0)
+            else if (Input.GetAxis("Use Power Horizontal Primary") < 0 || Input.GetAxis("Use Power Horizontal Secondary") < 0)
             {
                 m_direction = Vector3.left;
             }
-            else if (Input.GetAxis("Use Power Horizontal") > 0)
+            else if (Input.GetAxis("Use Power Horizontal Primary") > 0 || Input.GetAxis("Use Power Horizontal Secondary") > 0)
             {
                 m_direction = Vector3.right;
             }
             else
             {
                 m_direction = Vector3.zero;
+                m_UIArrow.texture = m_inactiveArrowTexture;
             }
+
+            // temp
+            m_UIArrow.transform.up = m_direction;
 
             // if the pegs aren't at the default position and the 'up beat' has just occured in the song
             if (!m_atDefaultPosition && m_timer > m_beatDelay * 0.5f)
             {
-                m_pegContainer.position = m_containerDefaultPosition; // lerp back
+                // return the pegs to their default position
+                m_pegContainer.position = m_containerDefaultPosition;
                 m_atDefaultPosition = true;
 
                 // temp
-                m_UIArrow.color = Color.white;
+                m_UIArrow.color = Color.blue;
             }
-            // if the 'down beat' has just occured in the song
-            else if (m_timer > m_beatDelay && m_timer < m_beatDelay + m_gracePeriod)
+            // if the pegs are at the default position and the 'down beat' has just occured in the song
+            else if (m_atDefaultPosition && m_timer > m_beatDelay && m_timer < m_beatDelay + m_gracePeriod)
             {
                 // temp
                 m_UIArrow.color = Color.yellow;
@@ -128,14 +194,14 @@ public class SashaPower : GreenPegPower
                 m_pegContainer.position = Vector3.Lerp(m_containerDefaultPosition, m_containerDefaultPosition + m_direction * m_moveDistance, (m_timer - m_beatDelay) * m_moveSpeed);
 
                 // if the time for the lerp has fully elapsed
-                if ((m_timer - m_beatDelay * m_moveSpeed) >= 1.0f)
+                if ((m_timer - m_beatDelay) * m_moveSpeed >= 1.0f)
                 {
                     // set the the pegs to the end position
                     m_pegContainer.position += m_direction * m_moveDistance;
                     // store that the pegs have moved
                     m_atDefaultPosition = false;
                     // reset the timer
-                    m_timer = 0.0f; // subtract not set
+                    m_timer -= m_beatDelay;
                 }
 
             }
--- Assets/Scripts/Isaac.cs
+++ Assets/Scripts/Isaac.cs
@@ -7,7 +7,7 @@ using UnityEngine;
     File name: Isaac.cs
     Summary: Manages the Player's ability to control Isaac's movement, shooting and bomb placement, as well as managing its limited duration
     Creation Date: 20/05/2024
-    Last Modified: 16/06/2025
+    Last Modified: 23/06/2025
 */
 public class Isaac : MonoBehaviour
 {
@@ -72,20 +72,20 @@ public class Isaac : MonoBehaviour
 		m_displacement = Vector2.zero;
 		
 		// determine the displacement of Isaac this frame
-        if (Input.GetAxis("Use Power Vertical") > 0)
+        if (Input.GetAxis("Use Power Vertical Primary") > 0)
 		{
 			m_displacement += Vector3.up * m_moveSpeed;
 		}
-		else if (Input.GetAxis("Use Power Vertical") < 0)
+		else if (Input.GetAxis("Use Power Vertical Primary") < 0)
 		{
 			m_displacement -= Vector3.up * m_moveSpeed;
 		}
 
-		if (Input.GetAxis("Use Power Horizontal") < 0)
+		if (Input.GetAxis("Use Power Horizontal Primary") < 0)
 		{
 			m_displacement -= Vector3.right * m_moveSpeed;
 		}
-		else if (Input.GetAxis("Use Power Horizontal") > 0)
+		else if (Input.GetAxis("Use Power Horizontal Primary") > 0)
 		{
 			m_displacement += Vector3.right * m_moveSpeed;
 		}
@@ -100,25 +100,25 @@ public class Isaac : MonoBehaviour
 		if (m_fireRateTimer >= m_fireRate)
 		{
 			// if Isaac should shoot a tear up
-			if (Input.GetAxis("Shoot Isaac's Tears Vertical") > 0)
+			if (Input.GetAxis("Shoot Isaac's Tears Vertical Secondary") > 0)
 			{
 				// shoot a tear up
 				ShootTear(Vector2.up);
 			}
 			// otherwise, if Isaac should shoot a tear down
-			else if (Input.GetAxis("Shoot Isaac's Tears Vertical") < 0)
+			else if (Input.GetAxis("Shoot Isaac's Tears Vertical Secondary") < 0)
 			{
 				// shoot a tear down
 				ShootTear(Vector2.down);
 			}
 			// otherwise, if Isaac should shoot a tear left
-			else if (Input.GetAxis("Shoot Isaac's Tears Horizontal") < 0)
+			else if (Input.GetAxis("Shoot Isaac's Tears Horizontal Secondary") < 0)
 			{
 				// shoot a tear left
 				ShootTear(Vector2.left);
 			}
 			// otherwise, if Isaac should shoot a tear right
-			else if (Input.GetAxis("Shoot Isaac's Tears Horizontal") > 0)
+			else if (Input.GetAxis("Shoot Isaac's Tears Horizontal Secondary") > 0)
 			{
 				// shoot a tear right
 				ShootTear(Vector2.right);
