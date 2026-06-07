b9727be564ab77263c8a863d0229c4516a04d540
Michael <michaelsweetman888@gmail.com>
2026-06-07 19:46:11 +1000

    Continued reworking BallOTron.cs
    Started rebuilding BallOTron.cs from the ground up
    The BallOTron.cs AddBall function creates a ball at a specified spawn height as before
    A new ball will now convert to a placeholder ball upon landing via the ConvertToPlaceholderBall function
    A placeholder ball have its physics and collision disabled, and will be added to a ball stack, which manages the physics and collisions of all included balls as one unit
    The ball stack will be resized and repositioned to accommodate for the new ball via the ResizeBallGroup function
    
    Time Spent This Week: 01:25:24

--- Assets/Scripts/UI/BallOTron.cs
+++ Assets/Scripts/UI/BallOTron.cs
@@ -6,10 +6,136 @@ using UnityEngine;
     File name: BallOTron.cs
     Summary: Manages the balls within the Ball-O-Tron UI display
     Creation Date: 19/01/2026
-    Last Modified: 01/06/2026
+    Last Modified: 07/06/2026
 */
 public class BallOTron : MonoBehaviour
 {
+
+    /*
+        first added ball is instantly made a placeholder since it starts with 0 velocity
+        subsequent ball collision with ball group collider not being recognised
+     */
+
+    public float m_spawnHeight = 4.5f;
+    public float m_destroyHeight = 5.0f;
+    public GameObject m_BallOTronBallPrefab;
+    public Transform m_ballHolder;
+    public float m_lowestAllowedVerticalVelocity = 0.01f;
+
+    Rigidbody2D m_ballGroupRigidbody;
+    BoxCollider2D m_ballGroupCollider;
+    Vector2 m_ballGroupColliderSize = Vector2.zero;
+    Stack<Rigidbody2D> m_balls;
+    Rigidbody2D m_newBall;
+    Collider2D m_newBallCollider;
+
+    private void OnCollisionEnter2D(Collision2D a_collision)
+    {
+        // TEMP
+        if (a_collision.gameObject == m_ballHolder)
+        {
+            
+        }
+
+        // if the collision object is the spawned ball
+        else /*TEMP*/ if (a_collision.gameObject == m_newBall && Mathf.Abs(m_newBall.velocity.y) < m_lowestAllowedVerticalVelocity)
+        {
+            ConvertToPlaceholderBall();
+        }
+    }
+
+    public void AddBall()
+    {
+        // create a new ball 
+        m_newBall = Instantiate(m_BallOTronBallPrefab, transform.parent).GetComponent<Rigidbody2D>();
+        // store its collider
+        m_newBallCollider = m_newBall.GetComponent<Collider2D>();
+        // position the ball at the spawn point
+        m_newBall.transform.position = transform.position + Vector3.up * m_spawnHeight;
+    }
+
+    void ResizeBallGroup()
+    {
+        // if the ball group collider is currently disabled but there are balls in the group
+        if (!m_ballGroupCollider.enabled && m_balls.Count > 0)
+        {
+            // enable the collider
+            m_ballGroupCollider.enabled = true;
+            // enable the ball group physics
+            m_ballGroupRigidbody.isKinematic = false;
+        }
+
+        // scale the collider size to the total height of each of the balls in the ball stack
+        m_ballGroupColliderSize.y = m_BallOTronBallPrefab.transform.localScale.y * m_balls.Count;
+        // apply the collider scale
+        m_ballGroupCollider.size = m_ballGroupColliderSize;
+        // position the ball group to be on top of the ball holder
+        transform.position = m_ballHolder.position + Vector3.up * (m_ballGroupColliderSize.y * 0.5f + m_ballHolder.localScale.y * 0.5f);
+    }
+
+    void ConvertToPlaceholderBall()
+    {
+        // stop physics from affecting the ball
+        m_newBall.isKinematic = true;
+        // disable the ball's collider
+        m_newBallCollider.enabled = false;
+        // position the ball on the top of the ball group
+        m_newBall.transform.position = transform.position + Vector3.up * (m_balls.Count * 0.5f/* + m_BallOTronBallPrefab.transform.localScale.y * 0.5f*/); // TEMP
+        // make the ball a child of the ball group
+        m_newBall.transform.parent = this.transform;
+        // add the ball to the ball group stack
+        m_balls.Push(m_newBall);
+        // stop storing this ball as the new ball
+        m_newBall = null;
+        // have the ball group collider resize to account for the new ball
+        ResizeBallGroup();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialise the m_balls stack
+        m_balls = new Stack<Rigidbody2D>();
+        // get the ball stack rigidbody
+        m_ballGroupRigidbody = GetComponent<Rigidbody2D>();
+        // get the ball stack collider
+        m_ballGroupCollider = GetComponent<BoxCollider2D>();
+        // store the initial ball group collider size
+        m_ballGroupColliderSize = m_ballGroupCollider.size;
+        // start with the ball group unaffected by phyiscs
+        m_ballGroupRigidbody.isKinematic = true;
+        // start with the ball group collider disabled
+        m_ballGroupCollider.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TEMP
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            AddBall();
+        }
+
+        // if there are currently no balls in the stack, a new ball has been spawned and it has stopped falling
+        if (m_balls.Count == 0 && m_newBall != null && Mathf.Abs(m_newBall.velocity.y) < m_lowestAllowedVerticalVelocity)
+        {
+            ConvertToPlaceholderBall();
+        }
+    }
+
+    // TEMP
+    public void LaunchBall()
+    {
+
+    }
+
+    public void SetBallCount(int a_ballCount)
+    {
+
+    }
+}
+/*
     public GameObject m_ballPrefab;
     public Rigidbody2D m_ballHolder;
     public float m_spawnHeight = -1.0f;
@@ -99,7 +225,8 @@ public class BallOTron : MonoBehaviour
         // if the ball's has effectively stopped
         if (m_topBall != null && m_topBall.velocity.sqrMagnitude < m_lowestAllowedVelocitySquared)
         {
-
+            m_topBall = null;
+            print("landed");
         }
         //
 
@@ -132,7 +259,6 @@ on launch
 top two balls are disconnected
 highest launches to top
 second most does slight bounce then reconnects
-
 
 
 this shouldn't be attached to a collider for deleting - no collider need exist, topball should be destroyed when high up enough
